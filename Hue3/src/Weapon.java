311fd5b53aaeb06ae0d82699fd9ecf47d8392ac5
Robin Haslinger <HaslingerR21080@sus.htl-grieskirchen.at>
2024-10-07 20:43:50 +0200

    changed Weapon Class

--- Hue3/src/Weapon.java
+++ Hue3/src/Weapon.java
@@ -6,40 +6,58 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Weapon {
-    public enum CombatType { MELEE, RANGED,NONE }
-    public enum DamageType { SLASHING, PIERCING, BLUNT,NONE }
-
+    private int strength;
+    private int value;
+    private int speed;
     private String name;
     private CombatType combatType;
     private DamageType damageType;
     private int damage;
 
-
-    public Weapon(String name, CombatType combatType, DamageType damageType, int damage) {
+    public Weapon(String name, CombatType combatType, DamageType damageType, int damage, int speed, int strength, int value) {
         this.name = name;
         this.combatType = combatType;
         this.damageType = damageType;
         this.damage = damage;
+        this.speed = speed;
+        this.strength = strength;
+        this.value = value;
     }
 
 
     public String getName() {
         return name;
     }
+
     public CombatType getCombatType() {
         return combatType;
     }
+
     public DamageType getDamageType() {
         return damageType;
     }
+
     public int getDamage() {
         return damage;
     }
 
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
     @Override
     public String toString() {
-        return String.format("%s | %s | %s | %d", name, combatType, damageType, damage);
+        return String.format("%s | %s | %s | %d | %d | %d | %d", name, combatType, damageType, damage, speed, strength, value);
     }
+
 
     public static List<Weapon> readWeaponsFromCSV(String filePath) {
         try (Stream<String> lines = Files.lines(Paths.get(filePath))) {
@@ -60,6 +78,13 @@ public class Weapon {
         CombatType combatType = CombatType.valueOf(fields[1].toUpperCase());
         DamageType damageType = DamageType.valueOf(fields[2].toUpperCase());
         int damage = Integer.parseInt(fields[3]);
-        return new Weapon(name, combatType, damageType, damage);
+        int speed = Integer.parseInt(fields[4]);
+        int strength = Integer.parseInt(fields[5]);
+        int value = Integer.parseInt(fields[6]);
+
+        return new Weapon(name, combatType, damageType, damage, speed, strength, value);
     }
+
+
+
 }
